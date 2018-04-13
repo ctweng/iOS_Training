1651cac1ca619edb3757439227fa59dda30e3dd4
ctweng <ctweng@yahoo-inc.com>
2018-04-13 10:05:55 +0800

    tab bar and navigation controller

---
+++ assignment3/HowDoYouTurnThisOn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HowDoYouTurnThisOn
+//
+//  Created by  Tim Weng on 4/10/18.
+//  Copyright © 2018 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ assignment3/HowDoYouTurnThisOn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HowDoYouTurnThisOn
+//
+//  Created by  Tim Weng on 4/10/18.
+//  Copyright © 2018 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
